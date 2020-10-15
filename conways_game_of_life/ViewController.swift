0826230f5f8b373890a3294664d893c84bb04b20
Craig Swanson <cswanson02@me.com>
2020-10-14 21:01:18 -0500

    Initial Commit

---
+++ conways_game_of_life/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  conways_game_of_life
+//
+//  Created by Craig Swanson on 10/14/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
