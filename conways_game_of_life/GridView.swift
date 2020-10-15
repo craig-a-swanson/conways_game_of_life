171873d96cd81f6203ff21e1fb536ca10f05248b
Craig Swanson <cswanson02@me.com>
2020-10-15 18:00:46 -0500

    Changed to custom drawing to adding labels for each cell. This is facilitate knowing which label was tapped at the start of the simulation.

--- conways_game_of_life/GridView.swift
+++ conways_game_of_life/GridView.swift
@@ -11,7 +11,11 @@ import UIKit
 class GridView: UIView {
 
     private let cellsPerRow: Int = 5
-    private let cellArray: [Bool] = [false, true, false, true, false, true, false, true, false, true, false, true, false, true, false, true, false, true, false, true, false, true, false, true, false]
+    private let cellArray: [Bool] = [false, true, false, true, false,
+                                     true, false, true, false, true,
+                                     false, true, false, true, false,
+                                     true, false, true, false, true,
+                                     false, true, false, true, false]
 
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -29,17 +33,36 @@ class GridView: UIView {
         let cellSize: CGFloat = gridSize / CGFloat(cellsPerRow)
 
         var index = 0
-        for row in 0..<cellsPerRow {
-            for column in 0..<cellsPerRow {
-                let cellSquare = CGRect(x: CGFloat(CGFloat(column) * cellSize), y: CGFloat(CGFloat(row) * cellSize), width: cellSize, height: cellSize)
+        for row in stride(from: CGFloat(0), through: CGFloat(cellsPerRow - 1), by: 1) {
+            for column in stride(from: CGFloat(0), through: CGFloat(cellsPerRow - 1), by:1) {
+//                create a row of labels with the appropriate size and background color
+//                set the tag on the label equal to Int(row * column)
+                let newCellLabel = UILabel(frame: CGRect(x: column * cellSize, y: row * cellSize, width: cellSize, height: cellSize))
+                newCellLabel.tag = Int((row + 1) * column)
+
                 var cellColor = UIColor.clear
                 if cellArray[index] {
                     cellColor = UIColor.black
                 }
                 index += 1
-                addRectangle(addRect: cellSquare, withColor: cellColor)
+                newCellLabel.backgroundColor = cellColor
+                addSubview(newCellLabel)
             }
         }
+//        for row in 0..<cellsPerRow {
+//            for column in 0..<cellsPerRow {
+//                let cellSquare = CGRect(x: CGFloat(CGFloat(column) * cellSize),
+//                                        y: CGFloat(CGFloat(row) * cellSize),
+//                                        width: cellSize,
+//                                        height: cellSize)
+//                var cellColor = UIColor.clear
+//                if cellArray[index] {
+//                    cellColor = UIColor.black
+//                }
+//                index += 1
+//                addRectangle(addRect: cellSquare, withColor: cellColor)
+//            }
+//        }
     }
 
     func addRectangle(addRect rectangle: CGRect, withColor color: UIColor) {
